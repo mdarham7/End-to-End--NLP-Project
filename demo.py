497a627b3cdcf47eb2739372659db3433904a109
Mohammed Arham <arhamuddin0@gmail.com>
2024-02-22 12:19:51 +0530

    notebook added

--- demo.py
+++ demo.py
@@ -1,4 +1,9 @@
 from hate.logger import logging
 from hate.exception import CustomException
+from hate.configuration.gcloud_syncer import GCloudSync
+import sys
 
 logging.info("Welcome to my Project")
+
+obj = GCloudSync()
+obj.sync_folder_from_gcloud("hate_speechproject","dataset.zip", "download/dataset.zip")
