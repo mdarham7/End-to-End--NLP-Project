89b57004c6ce5df37b742401e3251db232d5d8ad
Mohammed Arham <arhamuddin0@gmail.com>
2024-02-27 09:59:52 +0530

    Model Eval & Pusher Added

--- hate/constants/__init__.py
+++ hate/constants/__init__.py
@@ -49,3 +49,13 @@ MAX_LEN = 300
 LOSS = 'binary_crossentropy'
 METRICS = ['accuracy']
 ACTIVATION = 'sigmoid'
+
+#Model Evalution constants
+MODEL_EVALUATION_ARTIFACTS_DIR = 'ModelEvaluationArtifacts'
+BEST_MODEL_DIR = 'best_model'
+MODEL_EVALUATION_FILE_NAME = 'loss.csv'
+
+
+MODEL_NAME = 'model.h5'
+APP_HOST = "0.0.0.0"
+APP_PORT = 8080
